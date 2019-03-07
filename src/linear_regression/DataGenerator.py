fix gradient descent bug

src/linear_regression/DataGenerator.py
@@ -10,26 +10,29 @@ random.seed(1)
 
 class DataGenerator:
     _np_data = []
+    _theta = [-12, 5590, -0.002]
+    # _theta = [1, 1, 1]
 
-    @staticmethod
-    def _h(x1, x2):
-        return 1 + x1 + x2
+    def _h(self, x1, x2):
+        return self._theta[0] + self._theta[1] * x1 + self._theta[2] * x2
 
-    @staticmethod
-    def _generator():
+    def _generator(self):
         while True:
             x1 = random.uniform(-100, 100)
             x2 = x1 + random.uniform(-20, 20)
-            yield x1, x2, DataGenerator._h(x1, x2)
+            yield x1, x2, self._h(x1, x2)
 
     def generate(self, n):
         gen = self._generator()
         data = []
         for i in range(0, n):
             x1, x2, y = next(gen)
-            data.append([x1, x2, y + random.uniform(-50, 50)])
+            data.append([x1, x2, y + random.uniform(-(50 * self._theta[1]*self._theta[2]), (50 * self._theta[1]*self._theta[2]))])
         self._np_data = np.array(data, dtype=float)
-        return self._np_data
+        return self.get_data()
+
+    def get_data(self):
+        return np.copy(self._np_data)
 
     def plot(self):
         ax = plt.axes(projection='3d')

src/linear_regression/gradient_decent.py
@@ -4,10 +4,10 @@ from .compute_cost import compute_cost
 
 
 def gradient_decent(X, y, theta, alpha, num_iters):
-    n, m = X.shape
+    m, n = X.shape
     J_history = []
     for i in range(0, num_iters):
         errors = (X @ theta) - y
-        theta = theta - alpha / m * (np.transpose(X) @ errors)
+        theta = theta - alpha / m * (X.T @ errors)
         J_history.append(compute_cost(X, y, theta))
     return theta, J_history

src/linear_regression/main.py
@@ -3,13 +3,17 @@ import numpy as np
 from .DataGenerator import DataGenerator
 from .feature_normalize import feature_normalize
 from .gradient_decent import gradient_decent
+from .compute_cost import plot_cost
+
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     # step 1 - generate random data and plot
     generator = DataGenerator()
     data = generator.generate(500)
     generator.plot()
-    generator.save_data()
+    # generator.save_data()
 
     # step 2 - feature normalization
     m, n = data.shape
@@ -18,17 +22,32 @@ if __name__ == '__main__':
     y = data[:, n - 1]
     # print("X shape {}".format(X.shape))
     # print("y size {}".format(y.size))
-    X, mu, sigma = feature_normalize(X)
+    # X, mu, sigma = feature_normalize(X)
     # print(mu, sigma)
     X = np.insert(X, 0, np.ones(m), axis=1)
     # print(X)
 
     # step 3 - gradient decent
-    alpha = 0.01
-    num_iters = 50
+    alpha = 0.0002
+    num_iters = 10
     _, n = X.shape
     theta = np.zeros(n)
     theta, J_history = gradient_decent(X, y, theta, alpha, num_iters)
-    print('J_history: {}'.format(J_history))
+    # print('J_history: {}'.format(J_history))
     print('computed theta values:')
     print(theta)
+    plot_cost(J_history)
+
+    ax = plt.axes(projection='3d')
+
+    # draw line
+    x1 = np.linspace(-100, 100)
+    x2 = np.linspace(-100, 100)
+    ax.plot3D(x1, x2, theta[0] + theta[1] * x1 + theta[2] * x2, 'r')
+    # ax.plot3D(x1, x2, (1+x1+x2), 'b')
+    data = generator.get_data()
+    # draw points
+    ax.scatter3D(data[:, 0], data[:, 1], data[:, 2], 'ro')
+
+    plt.show()
+