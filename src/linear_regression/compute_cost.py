010c30c5b9d540b5f9bd5c6669395e11f85d9bf2
Vako Beridze <vako.beridze@gmail.com>
2019-03-07 17:35:50 +0400

    code refactor

--- src/linear_regression/compute_cost.py
+++ src/linear_regression/compute_cost.py
@@ -3,7 +3,6 @@ import numpy as np
 
 def compute_cost(X, y, theta):
     m = y.size
-    x_theta = np.dot(X, theta)
-    errors = x_theta - y
+    errors = (X @ theta) - y
     J = 1 / (2 * m) * (np.transpose(errors) @ errors)
     return J
